9fd28ec6359263b456d106118899f195ed6b0237
Sujitkrish <sujitkrishnanmsk@gmail.com>
2025-11-21 02:03:04 +0530

    looping task

---
+++ loop/for.py
@@ -0,0 +1,14 @@
+# a= int(input("Number: "))
+# def oddnum(a):
+#     for i in range (0,a+1):
+#         if(i%2!=0):
+#             print(f"{i} is odd")
+#         else:
+#             print(f"{i} is even")
+# oddnum(a)
+a= int(input("Number: "))
+def muli(a):
+    for i in range (0,11):
+        b = i*2
+        print(b)
+muli(a)
---
+++ loop/while.py
@@ -0,0 +1,4 @@
+a= 1
+while(a!=0):
+    a = int(input('num: '))
+print("thanks bro")
