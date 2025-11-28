7d36f79900fd345ef3b0c8e687abb2e88b875d9a
Sujitkrish <sujitkrishnanmsk@gmail.com>
2025-11-29 01:14:42 +0530

    string fun

---
+++ fun/string_function.py
@@ -0,0 +1,28 @@
+#a="bhuvi"
+
+#b=a.upper()
+#print(a.upper())
+#- Split the string "apple,banana,cherry" into a list of fruits using split().
+# a = "apple,banana,cherry"
+# b = a.split(",")
+# print(a)
+#- Replace all occurrences of "2024" with "2025" in the string "Happy New Year 2024".
+# a="Happy New Year 2024"
+# print(a.replace("2024","2025"))
+#- Write a program that checks if a string contains only digits using isdigit().
+#Example: "12345" → True, "123a" → False
+# a = "12345"
+# b = "123a"
+
+# print(a.isdigit())
+# print(b.isdigit())
+#- Given a sentence, count how many words it has using split() and len().
+#Example: "Python is fun to learn"
+
+# a = "Python is fun to learn"
+# b=a.split(" ")
+# print(b)
+# print(len(b))
+# - Write a program that finds the index of the first occurrence of "o" in "Hello World" using find().
+a="Hello World"
+print(a.find("l"))
